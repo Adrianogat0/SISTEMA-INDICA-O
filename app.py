43ad11bb30b8bf6658688174e846c2c2e721b8f5
DIANO <adrianovixian@gmail.com>
2025-05-29 13:44:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -172,7 +172,7 @@ def indicar_amigo():
         ''', (cliente_id, nome_amigo, '', 0))
         conn.commit()
         conn.close()
-      nome_cliente = session.get('cliente_nome')
+        nome_cliente = session.get('cliente_nome')
         mensagem = f"""👤 Indicado: *{nome_amigo}*
         
 *✅(1 Ano GRÁTIS) indicando amigos*
